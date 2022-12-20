791617d386af13d7d0dda932cb3b4feffd05cb23
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-12-20 16:51:26 +0530

    Wireframe UI and Basic Functionality

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+import addScore from './modules/addScore';
+import addImages from './modules/addImages';
+
+addScore();
+addImages();
---
+++ src/modules/addImages.js
@@ -0,0 +1,8 @@
+import footerImg from '../Img/footerImg.png';
+
+const addImages = () => {
+  const footer = document.getElementById('footerImg');
+  footer.src = footerImg;
+};
+
+export default addImages;
