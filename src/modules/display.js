47c90b54ba5004140e56a29d9894dc3468a657d6
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-12-22 16:05:36 +0530

    Fixed initial rendering on page load

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,9 +1,18 @@
-const display = () => {
+const display = async () => {
+  let scores = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Jun0BzNiB6z5IoA83KCo/scores/');
+  scores = await scores.json();
+    scores = scores.result;
+    scores.forEach((score) => {
+      scoresList.innerHTML += `
+      <li>${score.user}: ${score.score}</li>
+      `;
+    });
+  
   const refresh = document.getElementById('refresh');
   refresh.addEventListener('click', async () => {
     const scoresList = document.getElementById('scoresList');
     scoresList.innerHTML = '';
-    let scores = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Jun0BzNiB6z5IoA83KCo/scores/');
+    scores = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Jun0BzNiB6z5IoA83KCo/scores/');
     scores = await scores.json();
     scores = scores.result;
     scores.forEach((score) => {
