f581cab615e8d9160fdbf9a673f7ab746742234c
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-12-22 14:14:22 +0530

    Used apis for adding and displaying score

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,12 +1,16 @@
-import scores from './scores';
-
 const display = () => {
-  const scoresList = document.getElementById('scoresList');
-  scoresList.innerHTML = '';
-  scores.forEach((score) => {
-    scoresList.innerHTML += `
-    <li>${score.Name}: ${score.Score}</li>
-    `;
+  const refresh = document.getElementById('refresh');
+  refresh.addEventListener('click', async () => {
+    const scoresList = document.getElementById('scoresList');
+    scoresList.innerHTML = '';
+    let scores = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Jun0BzNiB6z5IoA83KCo/scores/');
+    scores = await scores.json();
+    scores = scores.result;
+    scores.forEach((score) => {
+      scoresList.innerHTML += `
+      <li>${score.user}: ${score.score}</li>
+      `;
+    });
   });
 };
 
