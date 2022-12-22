325a49802ba07b4887aaf31470f69ac2cf3fd7ec
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-12-22 16:10:23 +0530

    Fixed linter errors

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,16 +1,16 @@
 const display = async () => {
+  const scoresList = document.getElementById('scoresList');
   let scores = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Jun0BzNiB6z5IoA83KCo/scores/');
   scores = await scores.json();
-    scores = scores.result;
-    scores.forEach((score) => {
-      scoresList.innerHTML += `
+  scores = scores.result;
+  scores.forEach((score) => {
+    scoresList.innerHTML += `
       <li>${score.user}: ${score.score}</li>
       `;
-    });
-  
+  });
+
   const refresh = document.getElementById('refresh');
   refresh.addEventListener('click', async () => {
-    const scoresList = document.getElementById('scoresList');
     scoresList.innerHTML = '';
     scores = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Jun0BzNiB6z5IoA83KCo/scores/');
     scores = await scores.json();
