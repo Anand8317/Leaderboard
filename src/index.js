d933fa7e8400106e378be5e50e0202cb4ff92d07
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2022-12-23 22:20:49 +0530

    Added the style to the website

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
 import addScore from './modules/addScore';
-import addImages from './modules/addImages';
 import display from './modules/display';
 
 addScore();
-addImages();
 display();
